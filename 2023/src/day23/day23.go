10793335faedd862675f5c62904e8d7237fd4047
aethiiis <pubalexpub@gmail.com>
2025-01-01 23:51:22 +0100

    directory setup

---
+++ 2023/src/day23/day23.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func processing(filename string) {
+}
+
+func part1(filename string) int {
+	var result int
+	return result
+}
+
+func part2(filename string) int {
+	var result int
+	return result
+}
+
+func main() {
+	filename := "src/day23/input.txt"
+	fmt.Println(part1(filename))
+	fmt.Println(part2(filename))
+}
